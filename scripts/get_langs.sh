3a33ea97c290487254f375696d2e47bfb9f094ed
nirbheek <nirbheek@6618de09-9924-0410-a474-e320b3795f1b>
2009-07-02 07:25:54 +0000

    Fix get_langs.sh to handle en and en-US
    git-svn-id: svn://overlays.gentoo.org/proj/mozilla@228 6618de09-9924-0410-a474-e320b3795f1b

--- scripts/get_langs.sh
+++ scripts/get_langs.sh
@@ -5,6 +5,9 @@ if test -z "${1}"; then
 	exit 1
 fi
 
+INBUILT_LANGS="en en-US"
 VER="${1}"
 MIRROR_URI="http://releases.mozilla.org/pub/mozilla.org/firefox/releases/${VER}/linux-i686/xpi/"
-echo $(wget -q "${MIRROR_URI}" -O - | grep -o '[a-zA-Z-]\+\.xpi' | uniq | sed 's/\.xpi//')
+XPI_LANGS=$(wget -q "${MIRROR_URI}" -O - | grep -o '[a-zA-Z-]\+\.xpi' | uniq | sed 's/\.xpi//')
+LANGS=$(echo ${INBUILT_LANGS} ${XPI_LANGS} | tr " " "\n" | sort -d)
+echo ${LANGS}
