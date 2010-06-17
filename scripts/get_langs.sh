c68186491d3570faa9cbfa3991563fba8ae0820a
Nirbheek Chauhan <nirbheek@gentoo.org>
2010-06-17 22:40:35 +0530

    scripts/get_langs.sh: Allow langs of any product to be fetched

--- scripts/get_langs.sh
+++ scripts/get_langs.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-if test -z "${1}"; then
-	echo "Usage: ${0} FF_VER"
+if test -z "${2}"; then
+	echo "Usage: ${0} <product> <version>"
 	exit 1
 fi
 
 INBUILT_LANGS="en en-US"
-VER="${1}"
-MIRROR_URI="http://releases.mozilla.org/pub/mozilla.org/firefox/releases/${VER}/linux-i686/xpi/"
+PRODUCT="${1}"
+VER="${2}"
+MIRROR_URI="http://releases.mozilla.org/pub/mozilla.org/${PRODUCT}/releases/${VER}/linux-i686/xpi/"
 XPI_LANGS=$(wget -q "${MIRROR_URI}" -O - | grep -o '[a-zA-Z-]\+\.xpi' | uniq | sed 's/\.xpi//')
 LANGS=$(echo ${INBUILT_LANGS} ${XPI_LANGS} | tr " " "\n" | sort -d)
 echo ${LANGS}
